f4b7b2cbc8d55922fb7ed27c691779bc46013ac0
brit642 <cisse642@gmail.com>
2018-05-18 14:33:21 +0200

    Update app.js

--- app.js
+++ app.js
@@ -9,13 +9,22 @@ var app = express();
 app.use(express.static('public'));
 console.log('Exact name: ' + process.env.ORA_INSTANCE_NAME);
 runGetRequest();
-//runDatabaseQuery();
+runGetRequest1();
+runGetRequest2();
+runGetRequest3();
+runGetRequest4();
+runGetRequest5();
+runGetRequest6();
+runGetRequest7();
+runGetRequest8();
+runGetRequest9();
+runDatabaseQuery();
 
 // Does a GET request to ip.jsontest.com
 function runGetRequest() {
     
     //sample URL.
-    var url = "http://129.157.179.180:3000/shield/33/45/green/brit642";
+    var url = "http://129.157.179.180:3000/fighters/45/0/green/brit642;
     request(url, function(error, response, body) {
         if(!error) {
             console.log(body);
@@ -24,6 +33,124 @@ function runGetRequest() {
         }
     });
 };
+
+function runGetRequest1() {
+    
+    //sample URL.
+    var url = "http://129.157.179.180:3000/fighters/45/1/green/brit642;
+    request(url, function(error, response, body) {
+        if(!error) {
+            console.log(body);
+        } else {
+            console.log(error);
+        }
+    });
+};
+
+function runGetRequest2() {
+    
+    //sample URL.
+    var url = "http://129.157.179.180:3000/fighters/45/2/green/brit642;
+    request(url, function(error, response, body) {
+        if(!error) {
+            console.log(body);
+        } else {
+            console.log(error);
+        }
+    });
+};
+
+function runGetRequest3() {
+    
+    //sample URL.
+    var url = "http://129.157.179.180:3000/fighters/45/3/green/brit642;
+    request(url, function(error, response, body) {
+        if(!error) {
+            console.log(body);
+        } else {
+            console.log(error);
+        }
+    });
+};
+
+function runGetRequest4() {
+    
+    //sample URL.
+    var url = "http://129.157.179.180:3000/fighters/45/4/green/brit642;
+    request(url, function(error, response, body) {
+        if(!error) {
+            console.log(body);
+        } else {
+            console.log(error);
+        }
+    });
+};
+
+function runGetRequest5() {
+    
+    //sample URL.
+    var url = "http://129.157.179.180:3000/fighters/45/5/green/brit642;
+    request(url, function(error, response, body) {
+        if(!error) {
+            console.log(body);
+        } else {
+            console.log(error);
+        }
+    });
+};
+
+function runGetRequest6() {
+    
+    //sample URL.
+    var url = "http://129.157.179.180:3000/fighters/45/6/green/brit642;
+    request(url, function(error, response, body) {
+        if(!error) {
+            console.log(body);
+        } else {
+            console.log(error);
+        }
+    });
+};
+
+function runGetRequest7() {
+    
+    //sample URL.
+    var url = "http://129.157.179.180:3000/fighters/45/7/green/brit642;
+    request(url, function(error, response, body) {
+        if(!error) {
+            console.log(body);
+        } else {
+            console.log(error);
+        }
+    });
+};
+
+function runGetRequest8() {
+    
+    //sample URL.
+    var url = "http://129.157.179.180:3000/fighters/45/8/green/brit642;
+    request(url, function(error, response, body) {
+        if(!error) {
+            console.log(body);
+        } else {
+            console.log(error);
+        }
+    });
+};
+
+function runGetRequest9() {
+    
+    //sample URL.
+    var url = "http://129.157.179.180:3000/fighters/45/9/green/brit642;
+    request(url, function(error, response, body) {
+        if(!error) {
+            console.log(body);
+        } else {
+            console.log(error);
+        }
+    });
+};
+
 
 //Executes a SQL query
 function runDatabaseQuery() {
